8eea6e00a15e297ea2c6a4aaafccb19bc6f79b3a
carlospanarello <carlospanarello@gmail.com>
2015-10-14 11:51:26 -0300

    inclusao do porta aviao

--- BatalhaNaval/src/br/com/batalhanaval/Posicao.java
+++ BatalhaNaval/src/br/com/batalhanaval/Posicao.java
@@ -1,5 +1,35 @@
 package br.com.batalhanaval;
 
 public class Posicao {
+	private Linha linha;
+	private int coluna;
 	
+	public Posicao(Linha linha, int coluna) {
+		super();
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	private Posicao(int linha , int coluna){
+
+		for(Linha l : Linha.values()){
+			if(l.getNumero() == linha){
+				this.linha = l;
+				this.coluna = coluna;
+				break;
+			}
+		}
+	}
+
+	public Linha getLinha() {
+		return linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+	
+	public Posicao posicaoNova(int incrementoLinha, int incrementoColuna){
+		return new Posicao(this.getLinha().getNumero() + incrementoLinha, this.getColuna() + incrementoColuna);
+	}
 }
---
+++ BatalhaNaval/src/br/com/batalhanaval/Rotacao.java
@@ -0,0 +1,8 @@
+package br.com.batalhanaval;
+
+public enum Rotacao {
+	Direita,
+	Esquerda,
+	Cima,
+	Baixo;
+}
